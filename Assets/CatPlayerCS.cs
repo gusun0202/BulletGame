22924966bb78df013b7273449ae6b1d2dbd15f47
gusun0202 <66237335+gusun0202@users.noreply.github.com>
2020-06-04 16:26:33 +0900

    6.04심규선

---
+++ Assets/CatPlayerCS.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatPlayerCS : MonoBehaviour
+{
+    Rigidbody rigid;
+    void Start()
+    {
+        rigid = GetComponent<Rigodbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey.KeyCode.UpArrow)) {
+            Debug.Log("앞으로~");
+            transform.position = new Vector3(
+            transform.position.x
+            , transform.position.y
+            , transform.position.z + 3f * Time.delaTime
+            );
+        }
+        if (input.GetKay(KeyCode.DownArrow)) {
+            Debug.Log("뒤로~");
+            transform.position = new Vector3(
+            transform.position.x
+            , transform.position.y
+            , transform.position.z - 3f * Time.delaTime
+            );
+        }
+
+    }
+}
